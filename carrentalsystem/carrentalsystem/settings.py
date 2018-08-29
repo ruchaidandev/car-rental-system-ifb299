c5201d1b9b60d70320d1e328d375325766bbb944
Aidan Perera <n10109960@qut.edu.au>
2018-08-29 13:17:54 +1000

    Created application called CRCApp
    Changed the installed_apps with the application location.

--- carrentalsystem/carrentalsystem/settings.py
+++ carrentalsystem/carrentalsystem/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap4',
+    'crcapp.apps.CrcappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
