4204fc7f5370294c6a75b62e7f3f7471219c577d
Samuel <n9990186@qut.edu.au>
2018-09-01 14:21:11 +1000

    User table

--- carrentalsystem/crcapp/models.py
+++ carrentalsystem/crcapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    userID = models.IntergerField(primary_key=true)
+    userName = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+    userType = (('BM', 'Boardmember'),
+                ('SM','staff member'),
+                ('C','Customer'))
+    dateJoined = models.DateField()
+    lastLogin = models.DateField()    
