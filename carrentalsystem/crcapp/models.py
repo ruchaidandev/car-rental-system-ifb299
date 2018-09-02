8d7649f8e690afcc2e197b5c176805d07b7637d8
Samuel <n9990186@qut.edu.au>
2018-09-02 12:47:24 +1000

    Customer Database

--- carrentalsystem/crcapp/models.py
+++ carrentalsystem/crcapp/models.py
@@ -10,3 +10,18 @@ class Users(models.Model):
                 ('C','Customer'))
     dateJoined = models.DateField()
     lastLogin = models.DateField()
+
+class Customer(models.Model):
+    CustomerID = models.IntergerField(primary_key=true)
+    firstName = models.Charfield(maxlength=32)
+    lastName = models.Charfield(maxlength=32)
+    streetAddress = models.CharField(maxlength=32)
+    cityAddress = models.Charfield(maxlength=16)
+    postCodeAddress = models.IntergerField(maxlength = 4)
+    stateAddress = models.Charfield(maxlength = 3)
+    DOB = models.DateField()
+    driverLicenceNumber = models.IntergerField(maxlength = 9)
+    gender = (('M', 'Male'),
+              ('F', 'Female'),
+              ('NIL', 'Not Given'))
+    Occupation = 
