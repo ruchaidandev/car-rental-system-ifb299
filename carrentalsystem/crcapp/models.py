c28a6f66a62e0a899026b4ca3b0f2f134d093697
Samuel <n9990186@qut.edu.au>
2018-09-02 12:49:06 +1000

    Employee table

--- carrentalsystem/crcapp/models.py
+++ carrentalsystem/crcapp/models.py
@@ -13,15 +13,31 @@ class Users(models.Model):
 
 class Customer(models.Model):
     CustomerID = models.IntergerField(primary_key=true)
-    firstName = models.Charfield(maxlength=32)
-    lastName = models.Charfield(maxlength=32)
-    streetAddress = models.CharField(maxlength=32)
-    cityAddress = models.Charfield(maxlength=16)
-    postCodeAddress = models.IntergerField(maxlength = 4)
-    stateAddress = models.Charfield(maxlength = 3)
+    firstName = models.Charfield(max_length=32)
+    lastName = models.Charfield(max_length=32)
+    streetAddress = models.CharField(max_length=32)
+    cityAddress = models.Charfield(max_length=16)
+    postCodeAddress = models.IntergerField(max_length = 4)
+    stateAddress = models.Charfield(max_length = 3)
     DOB = models.DateField()
-    driverLicenceNumber = models.IntergerField(maxlength = 9)
+    driverLicenceNumber = models.IntergerField(max_length = 9)
     gender = (('M', 'Male'),
               ('F', 'Female'),
               ('NIL', 'Not Given'))
-    Occupation = 
+    Occupation = models.CharField(max_length=32)
+    phoneNumber = models.IntergerField(max_length = 10)
+    email = models.Charfield(max_length=64)
+
+
+class Employee(models.Model):
+    EmployeeID = models.IntergerField(primary_key=true)
+    firstName = models.Charfield(max_length=32)
+    lastName = models.Charfield(max_length=32)
+    streetAddress = models.CharField(max_length=32)
+    cityAddress = models.Charfield(max_length=16)
+    postCodeAddress = models.IntergerField(max_length = 4)
+    stateAddress = models.Charfield(max_length = 3)
+    DOB = models.DateField()
+    TFN = models.IntergerField(max_length=9)
+    phoneNumber = models.IntergerField(max_length = 10)
+    email = models.Charfield(max_length=64)
