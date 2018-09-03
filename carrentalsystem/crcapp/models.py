8529ebbb2dd3186ba8b342c68fd0817d5cedb34a
Aidan Perera <n10109960@qut.edu.au>
2018-09-03 22:43:33 +1000

    Changes for models data length and SQL file with records from previous system

--- carrentalsystem/crcapp/models.py
+++ carrentalsystem/crcapp/models.py
@@ -7,7 +7,7 @@ class Store(models.Model):
     storePhone = models.BigIntegerField()
     street = models.CharField(max_length=70)
     city = models.CharField(max_length=30)
-    state = models.IntegerField(max_length = 30)
+    state = models.CharField(max_length = 30)
 
 class Customer(models.Model):
     customerID = models.CharField(max_length=10, primary_key=True)
@@ -19,15 +19,13 @@ class Customer(models.Model):
     stateAddress = models.CharField(max_length = 30)
     DOB = models.DateField(auto_now=False, auto_now_add=False)
     driverLicenceNumber = models.IntegerField(max_length = 9)
-    gender = (('M', 'Male'),
-              ('F', 'Female'),
-              ('NIL', 'Not Given'))
-    Occupation = models.CharField(max_length=50)
+    gender = models.CharField(max_length=2)
+    occupation = models.CharField(max_length=50)
     phoneNumber = models.IntegerField(max_length = 10)
     email = models.EmailField()
     userName = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    userType = (('C','Customer'))
+    userType = models.CharField(max_length=16)
     dateJoined = models.DateField()
     lastLogin = models.DateField()
 
@@ -46,8 +44,7 @@ class Employee(models.Model):
     email = models.EmailField()
     userName = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    userType = (('BM', 'Boardmember'),
-                ('SM','staff'))
+    userType = models.CharField(max_length=16)
     dateJoined = models.DateField()
     lastLogin = models.DateField()
     storeID = models.ForeignKey(Store, on_delete=models.DO_NOTHING,blank=True,null=True)
@@ -55,18 +52,18 @@ class Employee(models.Model):
 
 class Vehicle(models.Model):
     vehicleID = models.CharField(max_length=10, primary_key=True)
-    makeName = models.CharField(max_length=30)
-    model = models.CharField(max_length=30)
-    series = models.CharField(max_length = 30)
+    makeName = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    series = models.CharField(max_length = 50)
     year = models.IntegerField(max_length=4)
     newPrice = models.DecimalField(max_digits=10,decimal_places=2)
     enginesize = models.DecimalField(max_digits=2,decimal_places=1)
     fuelSystem = models.CharField(max_length=20)
-    tankcapacity = models.DecimalField(max_digits=3,decimal_places=1)
-    power = models.IntegerField(max_length=3)
+    tankcapacity = models.DecimalField(max_digits=7,decimal_places=1)
+    power = models.IntegerField(max_length=5)
     seatingCapacity = models.IntegerField(max_length=2)
-    standardTransmission = models.CharField(max_length=6)
-    bodyType = models.CharField(max_length=20)
+    standardTransmission = models.CharField(max_length=10)
+    bodyType = models.CharField(max_length=30)
     driveType = models.CharField(max_length=3)
     wheelbase = models.IntegerField(max_length=4)
 
@@ -87,8 +84,7 @@ class Order(models.Model):
     orderDate = models.DateField(auto_now=False, auto_now_add=False)
     pickupDate = models.DateField(auto_now=False, auto_now_add=False)
     returnDate = models.DateField(auto_now=False, auto_now_add=False)
-    userType = (('Online', 'Online'),
-                ('In-Store','In-Store'))
+    orderType = models.CharField(max_length=16)
     customerID = models.ForeignKey(Customer, on_delete=models.DO_NOTHING,blank=True,null=True)
     pickupStoreID = models.ForeignKey(Store, related_name="pickupstore", on_delete=models.DO_NOTHING,blank=True,null=True)
     returnStoreID = models.ForeignKey(Store, related_name="returnstore", on_delete=models.DO_NOTHING,blank=True,null=True)
