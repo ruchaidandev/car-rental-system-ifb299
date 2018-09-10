55369225ac32051138912b788ea6dcdb20d438ef
Samuel <n9990186@qut.edu.au>
2018-09-10 13:43:46 +1000

    Employees Create + modify

--- carrentalsystem/crcapp/interfaces.py
+++ carrentalsystem/crcapp/interfaces.py
@@ -79,7 +79,67 @@ class Customer_():
         x = Customer.objects.get(customerID = ID)
         x.delete()
 
-    def search(arg):
-        if(arg == ""):
-            for each in Customer.objects.all()[0:20]:
-                print(each.firstName)
+#    def search(arg):
+#        if(arg == ""):
+#            for each in Customer.objects.all()[0:20]:
+#                print(each.firstName)
+
+class Employees_:
+    def create(employeeID_, firstName_, lastName_, streetAddress_, cityAddress_, postCodeAddress_, stateAddress_, DOB_, TFN_, phoneNumber_, email_, userName_, password_, userType_, dateJoined_, lastLogin_, storeID_):
+        x = Employee(employeeID = employeeID_, firstName = firstName_, lastName = lastName_, streetAddress = streetAddress_, cityAddress = cityAddress_, postCodeAddress = postCodeAddress_, stateAddress = stateAddress_, DOB = DOB_, TFN = TFN_, phoneNumber = phoneNumber_, email = email_, userName = userName_, password = password_, userType = userType_, dateJoined = dateJoined_, lastLogin = lastLogin_, storeID = storeID_)
+        x.save()
+
+    def modify(employeeID_, firstName_, lastName_, streetAddress_, cityAddress_, postCodeAddress_, stateAddress_, DOB_, TFN_, phoneNumber_, email_, userName_, password_, userType_, dateJoined_, lastLogin_, storeID_):
+
+        if(employeeID_ != ""):
+            x.employeeID = employeeID_
+
+        if(firstName_ != ""):
+            x.firstName = firstName_
+
+        if(lastName_ != ""):
+            x.lastName = lastName_
+
+        if(streetAddress_ != ""):
+            x.streetAddress = streetAddress_
+            
+        if(cityAddress_ != ""):
+            x.cityAddress = cityAddress_
+
+        if(postCodeAddress_ != ""):
+            x.postCodeAddress = postCodeAddress_
+
+        if(stateAddress_ != ""):
+            x.stateAddress = stateAddress_
+
+        if(DOB_ != ""):
+            x.DOB = DOB_
+
+        if(TFN_ != ""):
+            x.TFN = TFN_
+
+        if(phoneNumber_ != ""):
+            x.phoneNumber = phoneNumber_
+
+        if(email_ != ""):
+            x.email = email_
+
+        if(userName_ != ""):
+            x.userName = userName_
+
+        if(password_ != ""):
+            x.password = password_
+
+        if(userType_ != ""):
+            x.userType = userType_
+
+        if(dateJoined_ != ""):
+            x.dateJoined = dateJoined_
+
+        if(lastLogin_ != ""):
+            x.lastLogin = lastLogin_
+
+        if(storeID_ != ""):
+            x.storeID = storeID_
+
+        x.save()
