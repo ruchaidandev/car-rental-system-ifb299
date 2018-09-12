10146a06cb4e9634950db09f10c084ed32fb2c48
Aidan Perera <n10109960@qut.edu.au>
2018-09-12 12:11:32 +1000

    Added staff creation and organised the folder structure

--- carrentalsystem/crcapp/urls.py
+++ carrentalsystem/crcapp/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('login/employee', views.loginEmployee, name='login'),
     path('logoff', views.logoff, name='logoff'),
+    # For viewing the page
     path('staff/create', views.staffCreate, name='staffCreate'),
+    # Action class to add the values to the db
     path('create/staff', views.staffCreateAction, name='staffCreateAction'),
     
     path('sample', views.sample, name='staffCreateAction'),
