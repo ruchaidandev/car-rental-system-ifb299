a9218d8b70be5ecf9e8e4feef86cec8b4f2f90a5
JianHong_Lee(Jax) <jianhong.lee@connect.qut.edu.au>
2018-09-12 15:01:25 +1000

    Upgraded create customer webpage + linking of welcome page with create customer webpage

--- carrentalsystem/crcapp/urls.py
+++ carrentalsystem/crcapp/urls.py
@@ -7,8 +7,11 @@ urlpatterns = [
     path('home', views.home, name='home'),
     path('login/employee', views.loginEmployee, name='login'),
     path('logoff', views.logoff, name='logoff'),
+
     # For viewing the page
     path('staff/create', views.staffCreate, name='staffCreate'),
+    path('customer/create', views.customerCreate, name='customerCreate'),
+
     # Action class to add the values to the db
     path('create/staff', views.staffCreateAction, name='staffCreateAction'),
     path('booking/order', views.bookOrder, name='bookOrder'),
