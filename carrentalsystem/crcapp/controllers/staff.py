b5234bbb497d171ff065235c21ec78dc6e09afb1
Samuel <n9990186@qut.edu.au>
2018-09-17 08:47:27 +1000

    staff

--- carrentalsystem/crcapp/controllers/staff.py
+++ carrentalsystem/crcapp/controllers/staff.py
@@ -9,7 +9,7 @@ class StaffController:
 
     # Creating a new staff account. If a username exist in the employee
     # database, do not create the account. else, create the account for
-    # the employee 
+    # the employee
     def createStaff(request):
         staffObj = Employee.objects.raw("SELECT employeeID FROM `crcapp_employee` ORDER BY employeeID DESC LIMIT 1")[0]
         empID = staffObj.employeeID
@@ -36,21 +36,115 @@ class StaffController:
             lastLogin_ = timezone.now()
             storeID_ = request.POST.get("storeID")
             store = Store.objects.get(storeID=storeID_)
-            
+
             # Sam change this according to your create function for now Iam adding this to test
-            staff = Employee(employeeID = employeeID_, firstName = firstName_, lastName = lastName_, 
-            streetAddress = streetAddress_, cityAddress = cityAddress_, postCodeAddress = postCodeAddress_, 
-            stateAddress = stateAddress_, DOB = DOB_, TFN = TFN_, phoneNumber = phoneNumber_, email = email_, 
-            userName = userName_, password = password_, userType = userType_, dateJoined = dateJoined_, 
-            lastLogin = lastLogin_, storeID = store)
+            x = Employee(employeeID = employeeID_,
+                        firstName = firstName_,
+                        lastName = lastName_,
+                        streetAddress = streetAddress_,
+                        cityAddress = cityAddress_,
+                        postCodeAddress = postCodeAddress_,
+                        stateAddress = stateAddress_,
+                        DOB = DOB_,
+                        TFN = TFN_,
+                        phoneNumber = phoneNumber_,
+                        email = email_,
+                        userName = userName_,
+                        password = password_,
+                        userType = userType_,
+                        dateJoined = dateJoined_,
+                        lastLogin = lastLogin_,
+                        storeID = storeID_)
+
             vali = staff.full_clean()
             if vali:
                 return vali
             else:
                 staff.save()
                 return True
-            
+
             return False
-            
+
+        except ValidationError as e:
+            return e
+
+    def modify(employeeID_,
+               firstName_,
+               lastName_,
+               streetAddress_,
+               cityAddress_,
+               postCodeAddress_,
+               stateAddress_,
+               DOB_,
+               TFN_,
+               phoneNumber_,
+               email_,
+               userName_,
+               password_,
+               userType_,
+               dateJoined_,
+               lastLogin_,
+               storeID_):
+
+        x = Customer.objects.get(customerID_)
+
+        try:
+            if(firstName_ != ""):
+                x.firstName = firstName_
+
+            if(lastName_ != ""):
+                x.lastName = lastName_
+
+            if(streetAddress_ != ""):
+                x.streetAddress = streetAddress_
+
+            if(cityAddress_ != ""):
+                x.cityAddress = cityAddress_
+
+            if(postCodeAddress_ != ""):
+                x.postCodeAddress = postCodeAddress_
+
+            if(stateAddress_ != ""):
+                x.stateAddress = stateAddress_
+
+            if(DOB_ != ""):
+                x.DOB = DOB_
+
+            if(TFN_ != ""):
+                x.TFN = TFN_
+
+            if(phoneNumber_ != ""):
+                x.phoneNumber = phoneNumber_
+
+            if(email_ != ""):
+                x.email = email_
+
+            if(userName_ != ""):
+                x.userName = userName_
+
+            if(password_ != ""):
+                x.password = password_
+
+            if(userType_ != ""):
+                x.userType = userType_
+
+            if(dateJoined_ != ""):
+                x.dateJoined = dateJoined_
+
+            if(lastLogin_ != ""):
+                x.lastLogin = lastLogin_
+
+            if(storeID_ != ""):
+                x.storeID = storeID_
+
+            vali = staff.full_clean()
+            if vali:
+                return vali
+            else:
+                staff.save()
+                return True
+
+            return False
+
         except ValidationError as e:
             return e
