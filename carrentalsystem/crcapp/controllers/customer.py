7589f57461f7311d24ec2e7ff8c527b19ee46ed8
Samuel <n9990186@qut.edu.au>
2018-09-17 16:56:23 +1000

    customer search done

--- carrentalsystem/crcapp/controllers/customer.py
+++ carrentalsystem/crcapp/controllers/customer.py
@@ -135,4 +135,106 @@ class CustomerController:
     def search(arg):
         if(arg == ""):
             for each in Customer.objects.all():
-                print(each.all)
+                print(
+                each.customerID,
+                each.firstName,
+                each.lastName,
+                each.streetAddress,
+                each.cityAddress,
+                each.postCodeAddress,
+                each.stateAddress,
+                each.DOB,
+                each.driverLicenceNumber,
+                each.gender,
+                each.occupation,
+                each.phoneNumber,
+                each.email,
+                each.userName,
+                each.dateJoined,
+                each.lastLogin)
+
+        if(arg!= ""):
+            customerID_min = arg[0]
+            customerID_max = arg[1]
+            firstName = arg[2]
+            lastName = arg[3]
+            streetAdress = arg[4]
+            cityAddress = arg[5]
+            postCodeAddress = arg[6]
+            stateAddress = arg[7]
+            DOB_min = arg[8]
+            DOB_max = arg[9]
+            driverLicenceNumber = arg[10]
+            gender = arg[11]
+            occupation = arg[12]
+            phoneNumber = arg[13]
+            email = arg[14]
+            userName = arg[15]
+            dateJoined_min = arg[16]
+            dateJoined_max = arg[17]
+            lastLogin_min = arg[18]
+            lastLogin_max = arg[19]
+
+            condition = " "
+
+            if (customerID_min != ""):
+                condition = condition + "customerID >= \'" + customerID_min + "\' AND "
+            if (customerID_max != ""):
+                condition = condition + "customerID <= \'" + customerID_max + "\' AND "
+            if (firstName != ""):
+                condition = condition + "firstName = \'" + firstName + "\' AND "
+            if (lastName != ""):
+                condition = condition + "lastName = \'" + lastName + "\' AND "
+            if (streetAdress != ""):
+                condition = condition + "streetAdress = \'" + streetAdress + "\' AND "
+            if (cityAddress != ""):
+                condition = condition + "cityAddress = \'" + cityAddress + "\' AND "
+            if (postCodeAddress != ""):
+                condition = condition + "postCodeAddress = \'" + postCodeAddress + "\' AND "
+            if (stateAddress != ""):
+                condition = condition + "stateAddress = \'" + stateAddress + "\' AND "
+            if (DOB_min != ""):
+                condition = condition + "DOB >= \'" + DOB_min + "\' AND "
+            if (DOB_max != ""):
+                condition = condition + "DOB <= \'" + DOB_max + "\' AND "
+            if (driverLicenceNumber != ""):
+                condition = condition + "driverLicenceNumber = \'" + driverLicenceNumber + "\' AND "
+            if (gender != ""):
+                condition = condition + "gender = \'" + gender + "\' AND "
+            if (occupation != ""):
+                condition = condition + "occupation = \'" + occupation + "\' AND "
+            if (phoneNumber != ""):
+                condition = condition + "phoneNumber = \'" + phoneNumber + "\' AND "
+            if (email != ""):
+                condition = condition + "email = \'" + email + "\' AND "
+            if (userName != ""):
+                condition = condition + "userName = \'" + userName + "\' AND "
+            if (dateJoined_min != ""):
+                condition = condition + "dateJoined >= \'" + dateJoined_min + "\' AND "
+            if (dateJoined_max != ""):
+                condition = condition + "dateJoined <= \'" + dateJoined_max + "\' AND "
+            if (lastLogin_min != ""):
+                condition = condition + "lastLogin >= \'" + lastLogin_min + "\' AND "
+            if (lastLogin_max != ""):
+                condition = condition + "lastLogin <= \'" + lastLogin_max + "\' AND "
+
+            query = 'SELECT * FROM carrentaldb.crcapp_customer WHERE' + condition[:-5] +';'
+
+            for each in Customer.objects.raw(query):
+                print(
+                each.customerID,
+                each.firstName,
+                each.lastName,
+                each.streetAddress,
+                each.cityAddress,
+                each.postCodeAddress,
+                each.stateAddress,
+                each.DOB,
+                each.driverLicenceNumber,
+                each.gender,
+                each.occupation,
+                each.phoneNumber,
+                each.email,
+                each.userName,
+                each.dateJoined,
+                each.lastLogin)
