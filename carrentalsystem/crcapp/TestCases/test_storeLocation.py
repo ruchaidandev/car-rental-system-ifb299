6e55377a61c992229c513346ea5b5a59d7655c5b
JianHong Lee(Jax) <jianhong.lee@connect.qut.edu.au>
2018-10-19 17:33:36 +1000

    Update carrentalsystem/crcapp/TestCases/test_storeLocation.py

--- carrentalsystem/crcapp/TestCases/test_storeLocation.py
+++ carrentalsystem/crcapp/TestCases/test_storeLocation.py
@@ -11,6 +11,7 @@ import unittest, time, re, os, platform
 from crcapp.tests import BaseTest
 
 class StoreLocation(BaseTest):
+    # Test for store in staff webpage
     def test_storeLocation(self):
         driver = self.driver
         driver.get("http://127.0.0.1:8000/")
