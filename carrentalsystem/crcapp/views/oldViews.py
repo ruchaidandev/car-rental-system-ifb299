8552c6be5f5f9aba98ea40f144fde5bd337701ba
Kirby <kirbinator00@gmail.com>
2018-10-23 17:01:24 +1000

    Comments, general code quality things

--- carrentalsystem/crcapp/views/oldViews.py
+++ carrentalsystem/crcapp/views/oldViews.py
@@ -5,62 +5,6 @@ class LazyEncoder(DjangoJSONEncoder):
     def default(self, obj):
         return super().default(obj)
 
-# Developer: Aidan
-# Loading the index page
-def index(request, messages="", mtype="i"):
-
-    # Will get the session variables for the message
-    # and delete it after wards
-    msg = request.session.get('msg', '')
-    mtp = request.session.get('mtype', '')
-    if msg != "":
-        messages =  msg
-        del request.session['msg']
-    if mtp != "":
-        mtype = mtp
-        del request.session['mtype']
-    
-    stores = Store.objects.all()
-    vehicles = Vehicle.objects.order_by('-vehicleID')[:6]
-    return render(request, 'public/index.html', {'msg': messages, 'mtype': mtype, 'stores': stores, 'vehicles': vehicles})
-
-
-# Developer: Aidan
-# Login page 
-def loginIndex(request, messages="", mtype="i"):
-     # Will get the session variables for the message
-    # and delete it after wards
-    msg = request.session.get('msg', '')
-    mtp = request.session.get('mtype', '')
-    if msg != "":
-        messages =  msg
-        del request.session['msg']
-    if mtp != "":
-        mtype = mtp
-        del request.session['mtype']
-    return render(request, 'public/login.html', {'msg': messages, 'mtype': mtype})
-
-# Developer: Aidan
-# Customer Profile Page
-def profileIndex(request, messages="", mtype="i"):
-    if request.session.has_key('uid'):
-        name = request.session['name']
-        utype = request.session['utype']
-        
-        msg = request.session.get('msg', '')
-        mtype = request.session.get('mtp', '')
-        if msg != "":
-            messages =  msg
-            del request.session['msg']
-        if mtype != "":
-            del request.session['mtp']
-        customer = Customer.objects.get(customerID=request.session['uid'])
-        orders = Order.objects.filter(customerID=request.session['uid']).all()
-        
-        return render(request, 'public/profile.html', {'msg': messages, 'mtype': mtype,'name': name, 'utype': utype, 'customer': customer, 'orders':orders})
-    else:
-       return notLoggedIn(request)
-
 # Developer Aidan
 # Customer Profile Page
 def profile(request, messages="", mtype="i"):
@@ -81,44 +25,6 @@ def profile(request, messages="", mtype="i"):
     else:
        return notLoggedIn(request)
 
-# Developer: Aidan
-# Login page 
-def registerIndex(request, messages="", mtype="i"):
-    if request.method == 'POST':
-        form = request.POST
-        reason = CsrfViewMiddleware().process_view(request, None, (), {})
-        # If the reason is true it means verification failed
-        if reason:
-            return render(request, 'public/register.html', {'msg': 'Token verification failed!', 'mtype': "d"})
-        else:
-            if request.POST.get("password") == request.POST.get("confirmpassword"):
-               
-                result = customer.CustomerController.create(request, "signup")
-                if result == True:
-                    return render(request, 'public/register.html', {'msg': 'Account created, please login.', 'mtype': "i"})
-                elif result == False:
-                    return render(request, 'public/register.html', {'msg': 'Opps, something went wrong. Please try again.', 'mtype': "d"})
-                else:
-                    return render(request, 'public/register.html', {'msg': result, 'mtype': "a"})
-            else:
-                return render(request, 'public/register.html', {'msg': 'Sorry, the passwords did not match.', 'mtype': "i"})
-        return render(request, 'public/register.html', {'msg': messages, 'mtype': mtype})
-    else:
-
-        return render(request, 'public/register.html', {'msg': messages, 'mtype': mtype})
-
-# Developer: Jax
-# Login page 
-def storesIndex(request, messages="", mtype="i"):
-    NSW = Store.objects.filter(state = "New South Wales")
-    QLD = Store.objects.filter(state = "Queensland")
-    SA = Store.objects.filter(state = "South Australia")
-    TAS = Store.objects.filter(state = "Tasmania")
-    VIC = Store.objects.filter(state = "Victoria")
-
-    return render(request, 'public/stores.html', {'msg': messages, 'mtype': mtype, 'NSW': NSW, 'QLD': QLD, 'SA': SA, 'TAS' : TAS, 'VIC' : VIC })
-
-
 # Developer: Jax
 # Staff stores page 
 def storesStaff(request, messages="", mtype="i"):
@@ -129,37 +35,6 @@ def storesStaff(request, messages="", mtype="i"):
     VIC = Store.objects.filter(state = "Victoria")
 
     return render(request, 'store/storeLocation.html', {'msg': messages, 'mtype': mtype, 'NSW': NSW, 'QLD': QLD, 'SA': SA, 'TAS' : TAS, 'VIC' : VIC })
-
-# Developer: Aidan
-# get vehicle details to modify page
-def getVehicleIndex(request, option, msg='',mtype=''):
-    vehicle = Vehicle.objects.filter(vehicleID=option).values()[0]
-    if vehicle['storeID_id'] != "":
-        store = Store.objects.get(storeID = vehicle['storeID_id'] )
-    else:
-        store = { 'storeName' : "Not Found" }            
-    return render(request, 'public/vehicle.html', {'msg': '', 'mtype': mtype, 'vehicle':vehicle, 'store':store})
-
-
-# Developer: Aidan
-# Login page 
-def vehicleIndex(request, messages="", mtype="i"):
-    vehicles_list = Vehicle.objects.order_by('-vehicleID')
-    page = request.GET.get('page', 1)
-
-    paginator = Paginator(vehicles_list, 18)
-    try:
-        vehicles = paginator.page(page)
-    except PageNotAnInteger:
-        vehicles = paginator.page(1)
-    except EmptyPage:
-        vehicles = paginator.page(paginator.num_pages)
-
-    return render(request, 'public/vehicles.html', {'msg': messages, 'mtype': mtype, 'vehicles': vehicles })
-
-
-
-    
 
 # Developer: Aidan
 # Loading the home page
@@ -184,7 +59,6 @@ def home(request, messages=""):
     else:
        return notLoggedIn(request)
     
-
 # Developer: Aidan
 # Form action class for login, this will be used for employee login only
 def login(request):
@@ -219,7 +93,6 @@ def login(request):
         request.session['mtype'] = 'd'
         return redirect('/login')
 
-
 # Developer: Aidan
 @csrf_exempt # to disable csrf token check
 def disableStaff(request, option, empID):
@@ -238,10 +111,6 @@ def disableStaff(request, option, empID):
             return accessDeniedHome(request)
     else:
         return notLoggedIn(request)
-
-
-
-
 
 # Developer: Jax
 # Create staff member page
@@ -273,7 +142,6 @@ def staffCreate(request, messages="", mtype=""):
     else:
        return notLoggedIn(request)
 
-
 # Developer: Aidan
 # get all staff
 def getAllStaff(request, messages="", mtype=""):
@@ -288,7 +156,6 @@ def getAllStaff(request, messages="", mtype=""):
             accessDeniedHome(request)
     else:
        notLoggedIn(request)
-
 
 # Developer: Aidan
 # save changes of staff
@@ -332,8 +199,6 @@ def getStaff(request, option, msg='', mtype=''):
     else:
        return notLoggedIn(request)
 
-
-
 # Developer: Jax
 # Order Confirmation page
 def bookOrderConfirm(request, messages=""):
@@ -344,7 +209,6 @@ def bookOrderConfirm(request, messages=""):
         return render(request, 'booking/orderConfirm.html', {'msg': messages, 'name': name, 'utype': utype})
     else:
        return notLoggedIn(request)
-
 
 # Developer: Aidan
 # viewing the staff login management page
@@ -360,9 +224,7 @@ def viewStaffLogin(request):
             return accessDeniedHome(request)
     else:
        return notLoggedIn(request)
-       
-   
-   
+
 # Developer: Aidan
 # will return a json arrray to populate in datatable 
 # post variable will get the value sent by ajax
@@ -378,7 +240,6 @@ def getStaffFromStore(request):
     else:
         return HttpResponse("NULL")
 
-
 # Developer: Aidan
 # will return a json arrray of the login usernames
 @csrf_exempt # to disable csrf token check
@@ -391,20 +252,6 @@ def getUsernames(request):
        
     else:
         return HttpResponse("NULL")
-
-# Developer: Aidan
-# will return a json arrray of the login usernames
-@csrf_exempt # to disable csrf token check
-def getUsernamesCustomers(request):
-    # Checking session exists
-     
-    if request.method == 'POST':
-        customer = Customer.objects.all()
-        return HttpResponse(serialize('json', customer, cls=LazyEncoder))
-       
-    else:
-        return HttpResponse("NULL")
-
 
 # Developer: Aidan
 # get start details to the login page
@@ -421,7 +268,6 @@ def viewStaffLoginDetails(request, option, msg='',mtype=''):
             return accessDeniedHome(request)
     else:
        return notLoggedIn(request)
-
 
 # Developer: Jax
 # Searching for staff
@@ -719,36 +565,3 @@ def changeStaffProfilePassword(request):
             return accessDeniedHome(request) 
     else:
        return notLoggedIn(request)
-
-# Developer: Aidan
-# # Confirming order
-def confirmOrder(request):    
-
-    if request.session.has_key('bookings'):
-        booking = request.session.get('bookings', '')
-        
-        pickupStore = Store.objects.get(storeID= booking['pickUpStore'])
-        returnStore = Store.objects.get(storeID= booking['returnStore'])
-        vehicles = Vehicle.objects.filter(storeID = pickupStore )
-        returnDate = booking['pickupdate']
-        pickupDate = booking['returndate']
-        return render(request, 'public/booking.html', {'bookings': booking, 'vehicles': vehicles, 'returnStore': returnStore, 'pickupStore': pickupStore,
-     'pickupDate':pickupDate, 'returnDate':returnDate}) 
-    else:
-        return redirect("/")
-
-
-# Developer: Aidan
-# Setting stores to the order
-def setStore(request):
-    if request.method == 'POST':
-        reason = CsrfViewMiddleware().process_view(request, None, (), {})
-        # If the reason is true it means verification failed
-        if reason:
-            return redirect('/')
-        else:
-            bookings = { 'pickUpStore':request.POST.get("pickUpStore") , 'returnStore': request.POST.get("returnStore"),
-             'pickupdate':request.POST.get("pickupdate") , 'returndate': request.POST.get("returndate")}
-            request.session['bookings'] =bookings 
-            return redirect('/booking')
-    return redirect('/')
